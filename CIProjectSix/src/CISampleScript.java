05beeab1987ea96c2fc7807261edf21c7a8aceba
samsung <samsung@samsung-PC>
2021-08-08 18:00:21 +0530

    second version

---
+++ CIProjectSix/src/CISampleScript.java
@@ -0,0 +1,12 @@
+
+public class CISampleScript {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("continous Devops");
+		System.out.println("selenium");
+		System.out.println("java");
+		System.out.println("ashwin");
+	}
+
+}
